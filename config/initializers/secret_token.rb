aa38538e61551a61a666af5e9e819329e3f4c046
Frederico Guth <fredguth@cameron.local>
2012-04-16 17:56:30 -0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frescobol::Application.config.secret_token = '15ed829a965811f80fdb528e5e14d9b13d2cc6fe469c5efe4435575bf901cb4aeccb413d72c5760bcd04b25fa6da7520c765744b30bb4131103b15f8efd42a61'
